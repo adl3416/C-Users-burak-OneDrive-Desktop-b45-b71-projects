6d59c4ff92762886d3fbfa3aa4f06635415b9632
adl3416 <makara3416@gmail.com>
2022-04-13 16:43:46 +0200

    js

---
+++ 04-javascript/assets/js/65-sort-array.js
@@ -0,0 +1,26 @@
+const sSortAsc = (arr) => { 
+    return arr.sort((a,b)=> a.localeCompare(b));   /*  karakterele gore kaale al */
+}
+
+
+
+
+const sSortDesc = (arr) => { 
+    return arr.sort((a,b)=> b.localeCompare(a));
+}
+
+
+
+
+const nSortAsc = (arr) => { 
+    return arr.sort( (a,b) => a-b);
+}
+
+
+
+
+const nSortDesc = (arr) => { 
+    return arr.sort( (a,b) => b-a);
+}
+
+export {sSortAsc, sSortDesc, nSortAsc, nSortDesc}
