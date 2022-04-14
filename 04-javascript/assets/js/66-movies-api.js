6f1992087334b6543b93f4f45b8bf313d77c493e
adl3416 <makara3416@gmail.com>
2022-04-14 17:12:15 +0200

    js

--- 04-javascript/assets/js/66-movies-api.js
+++ 04-javascript/assets/js/66-movies-api.js
@@ -1,8 +1,11 @@
 
-const searchShow= (show) => {
+const searchShow = (show, cb) => { 
     fetch(`https://api.tvmaze.com/search/shows?q=${show}`)      /* burda endpointe baglaniyoruz */
-    .then(resp=> resp.json())                                  /* burda cevabi karsiliyoruz  cevabijson a gonderiyo oda log dayata gonderiyor*/
-    .then(data=> console.log(data));
-
+    .then(resp=> resp.json())                               /* burda cevabi karsiliyoruz  cevabijson a gonderiyo oda log dayata gonderiyor*/
+    .then(data=>{
+        cb(data);
+    });
 }
-export{searchShow}
+
+export {searchShow}
+
--- 04-javascript/assets/js/66-practise-feth.js
+++ 04-javascript/assets/js/66-practise-feth.js
@@ -1,6 +1,23 @@
 import {searchShow} from "./66-movies-api.js";
-
 document.querySelector("#txtSearch").addEventListener("input", (e)=>{
-    if(e.target.value.length<3) return;
-    searchShow(e.target.value);
+    const val = e.target.value;
+    if(val.length<3) return;
+    searchShow(val, (shows)=>{
+        let htmlShows = "";
+        shows.forEach(item => {
+            console.log(item);
+            htmlShows += `
+            <div class="col-md-6 col-lg-4 p-3">
+                <div class="card">
+                    <img src="${item.show.image ? item.show.image.medium : "assets/img/no-image.png"}" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        document.querySelector("#tvshows").innerHTML = htmlShows;
+    });
 });
