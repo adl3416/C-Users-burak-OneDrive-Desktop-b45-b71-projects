e02266805b05ec2d4bef58dfd25939064aa610e0
adl3416 <makara3416@gmail.com>
2022-04-22 16:57:59 +0200

    dersarasi

--- 05-reactjs/cv/src/App.js
+++ 05-reactjs/cv/src/App.js
@@ -6,6 +6,7 @@ import Intro from './components/intro/intro';
 import Abaut from './components/abaut/abaut';
 import Quote from './components/quote/quote';
 import Skills from './components/skills/skills';
+import Experiences from './components/experience/experiences';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Abaut/>
       <Quote/>
       <Skills/>
+      <Experiences/>
     </>
   );
 }
